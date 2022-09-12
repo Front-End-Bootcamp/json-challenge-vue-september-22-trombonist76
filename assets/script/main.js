c6e74a9d365d32d665c5319f6c80b30c7891f6ac
trombonist76 <mahmut.hg@gmail.com>
2022-09-12 15:33:04 +0300

    [Update] Ödev-1 yüklendi.

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,30 @@
-console.log('👻');
+import data from "../data.json"
+
+function getGroups(array){
+  const result = array.reduce((groupedObj,personObj)=>{
+    const {group,name,type} = personObj
+
+    if(!(group in groupedObj)){
+      groupedObj[group] = {student:[],assistant:""}
+    }
+
+    if(type === "assistant"){
+      groupedObj[group].assistant = name
+      
+    }else{
+      groupedObj[group].student.push(name)
+    }
+
+    return groupedObj
+  },{})
+
+  return result
+}
+
+function getGroupStudents(array,groupName){
+  return getGroups(array)[groupName].student
+}
+
+const student = getGroupStudents(data,"Orchid")
+console.log("Student:", student)
+
