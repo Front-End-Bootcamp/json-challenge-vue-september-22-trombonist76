c24146f031e3713d1b93b94e972f105afd7e387a
trombonist76 <mahmut.hg@gmail.com>
2022-09-13 10:49:46 +0300

    [Update] Son güncellemeler yapıldı.

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,35 +2,25 @@ import data from "../data.json"
 
 function getGroups(personArray){
   //Reduce methoduyla boş bir obje oluşturdum -groupedObj-. Fonksiyon kişi bilgilerini -personObj- değişkeniyle döndü.  
-  
   return personArray.reduce((groupedObj,personObj)=>{
     const {group,name,type} = personObj
-
-  //Burada kişinin grubu, daha önce boş olan objeye -groupedObj- eklenmediyse aşağıdaki gibi key,value olarak ekleniyor.
-    if(!(group in groupedObj)){
-      groupedObj[group] = {student:[],assistant:""}
-    }
+    const roleCondition = type === "assistant"
+    
+    //Burada kişinin grubu, daha önce boş olan objeye -groupedObj- eklenmediyse aşağıdaki gibi key,value olarak ekleniyor.
+    if(!(group in groupedObj)) groupedObj[group] = {student:[],assistant:""}  //Orchid : {student:[], assistant:""}
 
   //Burada ise kişi asistan ise objenin -assistant- prop'una ekleniyor. Katılımcı ise student dizisine ekleniyor.
-    if(type === "assistant"){
-      groupedObj[group].assistant = name
-      
-    }else{
-      groupedObj[group].student.push(name)
-    }
+    roleCondition 
+    ? groupedObj[group].assistant = name 
+    : groupedObj[group].student.push(name)
 
     return groupedObj
   },{})
 }
 
-function getGroupStudents(personArray,groupName){
-  //Parametre olarak ismi alınan gruba ait bilgileri getiriyor.
-  return getGroups(personArray)[groupName] 
-}
-
-
+let selectedGroupName = "Orchid"
 const allGroups = getGroups(data)
-const selectedGroup = getGroupStudents(data,"Orchid")
+const selectedGroup = allGroups[selectedGroupName]
 console.log("All groups", allGroups)
 console.log("Selected Group", selectedGroup)
 
